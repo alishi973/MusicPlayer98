e983f248d88a1f723a6cf0621c66d869c6b7dd73
ali shirzadeh <shirzadehali973@gmail.com>
2020-06-02 17:21:46 +0430

    Updates

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -33396,7 +33396,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64601" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61790" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
