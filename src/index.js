6bc8e6809f5a4d513616aba7fad9bae7a0cb9a3e
ali <shirzadehali973@gmail.com>
2020-06-13 22:59:20 +0430

    Migrate To SCSS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import '98.css';
-import './style.css';
+// import './style.css';
+import './styles.scss';
 
 render(<App />, document.getElementById('root'));
